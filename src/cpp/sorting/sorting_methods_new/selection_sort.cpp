079f82d82d40cb5022898253dde9d3f02fadabc4
Harsh Raj <keshriayush321@gmail.com>
2026-07-21 18:51:16 +0530

    andcv

---
+++ src/cpp/sorting/sorting_methods_new/selection_sort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+// Find the minimum and then swap and make the minimum element to the extreme left
+
+void SelectionSort(vector<int> &arr, int n){
+    for (int i = 0; i < n-1; i++) {
+        int mini = i;
+        for (int j = i+1; j < n; j++) {
+            if (arr[j] < arr[mini]) mini = j;
+        }
+        swap(arr[mini],arr[i]);
+    }
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+    vector <int> v;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        v.emplace_back(x);
+    }
+    SelectionSort(v,n);
+    for (auto it : v) cout << it << " ";
+    return 0;
+}
