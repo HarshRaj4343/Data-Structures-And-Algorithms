6404ce3f61bb9670762fceaba017c9caddb4db24
Harsh Raj <keshriayush321@gmail.com>
2026-07-22 14:13:56 +0530

    vhdsH

--- src/cpp/sorting/sorting_methods_new/bubble_sort.cpp
+++ src/cpp/sorting/sorting_methods_new/bubble_sort.cpp
@@ -3,18 +3,16 @@
 #include <vector>
 using namespace std;
 
-// Push the max element to the last by adjacent swaps
-
 void BubbleSort(vector<int> &arr, int n) {
-    for (int i = n - 1; i > 0; i--) {
+    for (int i = 0; i < n-1; i++) {
         bool swapped = false;
-        for (int j = 0; j < i; j++) {
-            if (arr[j] > arr[j + 1]) {
-                swap(arr[j], arr[j + 1]);
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]){
+                swap(arr[j],arr[j+1]);
                 swapped = true;
             }
         }
-        if (!swapped) break; // already sorted, no need to continue
+        if (!swapped) break;
     }
 }
     
