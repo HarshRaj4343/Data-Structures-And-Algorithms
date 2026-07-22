c66618d02245a1c622eb7a7cee4c7e029864214d
Harsh Raj <keshriayush321@gmail.com>
2026-07-22 14:14:17 +0530

    winFAS

--- src/cpp/sorting/sorting_methods_new/selection_sort.cpp
+++ src/cpp/sorting/sorting_methods_new/selection_sort.cpp
@@ -9,7 +9,9 @@ void SelectionSort(vector<int> &arr, int n){
     for (int i = 0; i < n-1; i++) {
         int mini = i;
         for (int j = i+1; j < n; j++) {
-            if (arr[j] < arr[mini]) mini = j;
+            if (arr[mini] > arr[j]){
+                mini = j;
+            }
         }
         swap(arr[mini],arr[i]);
     }
