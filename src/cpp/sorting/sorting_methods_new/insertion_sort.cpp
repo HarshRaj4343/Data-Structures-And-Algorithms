486189455f1a2069e1ffb3dadc346fcf694e01bf
Harsh Raj <keshriayush321@gmail.com>
2026-07-22 14:13:36 +0530

    DN

--- src/cpp/sorting/sorting_methods_new/insertion_sort.cpp
+++ src/cpp/sorting/sorting_methods_new/insertion_sort.cpp
@@ -4,8 +4,15 @@
 using namespace std;
 
 void InsertionSort(vector<int> &arr, int n) {
-    for (int i = 0; i < n; i++) {
-        
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];      
+        int j = i - 1;        
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;      
     }
 }
     
