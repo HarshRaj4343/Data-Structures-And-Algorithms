8e74c0b9d8d1989763a089379dfebc10f1634e14
Harsh Raj <keshriayush321@gmail.com>
2026-07-25 17:15:01 +0530

    Wfdscnjdv

--- src/cpp/arrays/array_basix/union_sorted_arrays.cpp
+++ src/cpp/arrays/array_basix/union_sorted_arrays.cpp
@@ -1,42 +1,42 @@
 // Brute Force
 
-// #include <iostream>
-// #include <vector>
-// #include <set>
-// using namespace std;
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
 
-// set <int> UnionFind(vector <int> &arr1, vector <int> &arr2, int n, int m){
-//     set <int> s;
-//     for (int i = 0; i < n; i++) {
-//         s.insert(arr1[i]);
-//     }
-//     for (int i = 0; i < m; i++) {
-//         s.insert(arr2[i]);
-//     }
-//     return s;
-// }
+set <int> UnionFind(vector <int> &arr1, vector <int> &arr2, int n, int m){
+    set <int> s;
+    for (int i = 0; i < n; i++) {
+        s.insert(arr1[i]);
+    }
+    for (int i = 0; i < m; i++) {
+        s.insert(arr2[i]);
+    }
+    return s;
+}
 
-// int main()
-// {
-//     int n,m;
-//     cin >> n >> m;
-//     vector<int> v;
-//     vector<int> b;
-//     for (int i = 0; i < n; i++)
-//     {
-//         int x;
-//         cin >> x;
-//         v.emplace_back(x);
-//     }
-//     for (int i = 0; i < m; i++) {
-//         int y;
-//         cin >> y;
-//         b.emplace_back(y);
-//     }
-//     set <int> sr = UnionFind(v,b,n,m);
-//     for (auto it: sr) cout << it << " ";
-//     return 0;
-// } // do not use unordered set
+int main()
+{
+    int n,m;
+    cin >> n >> m;
+    vector<int> v;
+    vector<int> b;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        v.emplace_back(x);
+    }
+    for (int i = 0; i < m; i++) {
+        int y;
+        cin >> y;
+        b.emplace_back(y);
+    }
+    set <int> sr = UnionFind(v,b,n,m);
+    for (auto it: sr) cout << it << " ";
+    return 0;
+} // do not use unordered set
 
 // O(nlogn) + O(mlogm) + O(n+m) -> TC
 // O(n+m) + O(n+m) -> last one for returning -> SC
@@ -101,3 +101,6 @@ int main()
     for (auto it : ans) cout << it << " ";
     return 0;
 }
+
+// TC = O(n+m)
+// SC = O(n+m) - Just for returning the answer
