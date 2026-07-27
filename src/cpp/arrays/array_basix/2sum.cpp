6e1e51a5b34f9a3c39326789cae5ffcc5d15f982
Harsh Raj <keshriayush321@gmail.com>
2026-07-27 18:39:10 +0530

    erjsa

--- src/cpp/arrays/array_basix/2sum.cpp
+++ src/cpp/arrays/array_basix/2sum.cpp
@@ -1,6 +1,8 @@
 // brute force
 
 #include <iostream>
+#include <algorithm>
+#include <map>
 #include <vector>
 using namespace std;
 bool twosumvariant1(vector<int> &arr, int n, int target)
@@ -17,6 +19,7 @@ bool twosumvariant1(vector<int> &arr, int n, int target)
             }
         }
     }
+    return false;
 }
 
 vector<int> twosumvariant2(vector<int> &arr, int n, int target)
@@ -55,6 +58,75 @@ vector<int> twosumvari2optimised(vector<int> &arr, int n, int target)
     return ans;
 }
 
+// using hashing
+
+bool twosumbetter(vector<int> &arr, int n, int target)
+{
+    map<int, int> mp;
+    for (int i = 0; i < n; i++)
+    {
+        int a = arr[i];
+        int residual = target - a;
+        if (mp.find(residual) != mp.end())
+        {
+            return true;
+        }
+        mp[a] = i;
+    }
+    return false;
+}
+
+// TC = O(nlogn)
+// SC = O(n)
+// similarly, u can also print the indices also
+
+// A slightly better solution using 2 pointer greedy approach
+
+bool twosumoptimal(vector<int> &arr, int n, int target)
+{
+    sort(arr.begin(), arr.end());
+    int left = 0;
+    int right = n - 1;
+    while (left < right)
+    {
+        int sum = arr[left] + arr[right];
+        if (sum == target)
+            return true;
+        else if (sum < target)
+            left++;
+        else
+            right--;
+    }
+    return false;
+}
+
+// TC = O(n) + O(nlogn)
+// SC = O(1) without distorting the array
+
+// Optimal Version 2 - not preferred for variant 2
+
+// vector <int> twosumopt(vector <int> &arr, int n, int target){
+//     vector <int> ans;
+//     map <int,int> mpp;
+//     int left = 0;
+//     int right = n-1;
+//     while (left<right){
+//         int sum = arr[left] + arr[right];
+//         if (sum == target){
+//             ans.emplace_back(mpp[a]);
+//             ans.emplace_back(mpp[b]);
+//         }
+//         else if (sum < target)
+//             left++;
+//         else
+//             right--;
+//     }
+//     return ans;
+// }
+
+// retry this method while revision
+
+
 int main()
 {
     int n, target;
@@ -72,7 +144,14 @@ int main()
         cout << it << " ";
     vector<int> ans1 = twosumvari2optimised(v, n, target);
     cout << "\n";
-    for (auto it2 : ans)
+    for (auto it2 : ans1)
         cout << it2 << " ";
+    cout << "\n";
+    cout << twosumbetter(v, n, target) << "\n";
+    cout << twosumoptimal(v, n, target) << "\n";
+    vector<int> ans2 = twosumopt(v, n, target);
+    cout << "\n";
+    for (auto it3 : ans2)
+        cout << it3 << " ";
     return 0;
 }
