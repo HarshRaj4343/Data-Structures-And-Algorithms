3fee057f5476a4a0aea30ab8de2a0677abeead9f
Harsh Raj <keshriayush321@gmail.com>
2026-07-28 19:05:22 +0530

    lll

---
+++ src/cpp/binary_search/bs_basix/introduction.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+// input is already given (sorted)
+
+int binarysearchiterative(vector <int> &arr, int n, int target){
+    int low = 0;
+    int high = n-1;
+    while (low <= high){
+        int mid = (low+high)/2;
+        if (arr[mid] == target) return 1;
+        else if (arr[mid] < target) low = mid + 1;
+        else high = mid - 1;
+    }
+    return -1;
+}
+
+int binarysearchrecursive(vector <int> &arr, int low, int high, int target){
+    if (low > high) return -1;
+    int mid = (low+high)/2;
+    if (arr[mid] == target) return 1;
+    else if (arr[mid] < target) return binarysearchrecursive(arr, mid+1,high,target);
+    else return binarysearchrecursive(arr,low, mid-1, target);
+}
+
+int main()
+{
+    int n,target;
+    cin >> n >> target;
+    vector<int> v;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        v.emplace_back(x);
+    }
+    cout << binarysearchiterative(v , n , target) << endl;
+    cout << binarysearchrecursive(v , 0, n-1, target) << endl;
+    return 0;
+}
