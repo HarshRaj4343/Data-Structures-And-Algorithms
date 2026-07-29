d6d167e2b5ef1baaf42e4b71458244dfeac1507a
Harsh Raj <keshriayush321@gmail.com>
2026-07-29 19:19:31 +0530

    brute force

--- src/cpp/binary_search/bs_basix/first_last_occurence.cpp
+++ src/cpp/binary_search/bs_basix/first_last_occurence.cpp
@@ -2,10 +2,31 @@
 #include <vector>
 using namespace std;
 
+// array is given sorted
+vector <int> bruteforce(vector <int> &arr, int n, int x){
+    vector <int> ans;
+    int first = -1;
+    int last = -1;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == x){
+            if (first == -1) first = i;
+            last = i;
+        }
+    }
+    ans.emplace_back(first);
+    ans.emplace_back(last);
+    return ans;
+}
+// TC = O(N)
+
+vector <int> binarysearch(vector <int> &arr, int n, int x){
+    vector <int> ans;
+}
+
 int main()
 {
-    int n;
-    cin >> n;
+    int n,x;
+    cin >> n >> x;
     vector<int> v;
     for (int i = 0; i < n; i++)
     {
@@ -13,6 +34,8 @@ int main()
         cin >> x;
         v.emplace_back(x);
     }
-    
+    vector <int> ans1 = bruteforce(v,n,x);
+    for (auto it : ans1) cout << it << " ";
+    cout << "\n";
     return 0;
 }
