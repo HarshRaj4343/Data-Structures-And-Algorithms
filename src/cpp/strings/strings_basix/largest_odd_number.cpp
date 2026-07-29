ea2d28e58b49b79119a544564900a1d4f950fb94
Harsh Raj <keshriayush321@gmail.com>
2026-07-29 22:40:12 +0530

    sdfv

--- src/cpp/strings/strings_basix/largest_odd_number.cpp
+++ src/cpp/strings/strings_basix/largest_odd_number.cpp
@@ -1,12 +1,36 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
 #include <vector>
 using namespace std;
+
+
+int oddnum(string s){
+    int n = s.length();
+    int maxodd = 1;
+    for (int i = 0; i < n; i++) {
+        char c = s[i];
+        int digit = c - '0';
+        int odd = 1;
+        if (digit % 2 != 0) {
+            odd = digit;
+            maxodd = max(odd,maxodd);
+        }
+    }
+    return maxodd;
+}
+
 
 int main()
 {
     string s;
     cin >> s;
     int n = s.length();
-    
+    cout << oddnum(s) << " ";
     return 0;
 }
+
+// std::stoi (returns int)
+// std::stol (returns long)
+// std::stoll (returns long long)
+// std::stoull (returns unsigned long long)
