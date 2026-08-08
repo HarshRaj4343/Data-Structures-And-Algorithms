43a912e29fe8df232d954c33baeeac572611702e
Harsh Raj <keshriayush321@gmail.com>
2026-08-08 15:54:27 +0530

    hcgfgh

--- src/cpp/binary_search/bs_basix/floor_ceil_in_sorted.cpp
+++ src/cpp/binary_search/bs_basix/floor_ceil_in_sorted.cpp
@@ -10,18 +10,19 @@ int ceil(vector<int> &arr, int n, int x)
 {
     int low = 0;
     int high = n - 1;
+    int ans = n;
     while (low <= high)
     {
         int mid = (low + high) / 2;
         if (arr[mid] >= x)
         {
+            ans = arr[mid];
             high = mid - 1;
-            return arr[mid];
         }
         else
             low = mid + 1;
     }
-    return -1;
+    return ans;
 }
 
 int floor(vector <int> &arr, int n, int x){
