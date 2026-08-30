1bafc579d86b1142c48084856d7aafdeb33db02e
Harsh Raj <keshriayush321@gmail.com>
2026-08-30 12:14:55 +0530

    queues

---
+++ src/cpp/Stacks-Queues/stacks.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+// Stacks are LIFO
+// Queues are FIFO
+
+class Stack {
+    int st[10];
+    int top = -1;
+
+public:
+    void push(int x) {
+        if (top >= 9) {
+            cout << "Stack Overflow" << endl;
+            return;
+        }
+        top++;
+        st[top] = x;
+    }
+
+    void peek() {
+        if (top == -1) {
+            cout << "Stack is Empty" << endl;
+            return;
+        }
+        cout << st[top] << endl;
+    }
+
+    void pop() {
+        if (top == -1) {
+            cout << "Stack is Empty" << endl;
+            return;
+        }
+        top--;
+    }
+
+    void size() {
+        cout << top + 1 << endl;
+    }
+};
+
+int main() {
+    Stack s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    s.peek(); 
+    s.pop();
+    s.peek();  
+
+    s.size();  
+
+    return 0;
+}
